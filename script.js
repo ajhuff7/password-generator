5e8c18b93a2581b667392ca7ea6656fdd3306467
ajhuff7 <70546019+ajhuff7@users.noreply.github.com>
2020-09-19 23:22:20 -0700

    password logic

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function randomizer(arr){
  }
 
 function generatePassword(){
-    var randos = [] 
-    var selected = []
+    var select = []
+    var newChars = []
     var newpassword = []
 
     var pLength = prompt("What is the desired length of the password?")
@@ -36,36 +36,33 @@ function generatePassword(){
         alert("Must choose atleast one character type.");
     }
 
-    if (pLower){
-        randos = randos.concat(lowers);  
-        selected.push(randomizer(lowers));
+    if (pLower){  
+        select.push(lowers.join(""));
     }
 
-    if (pUpper){
-        randos = randos.concat(uppers);  
-        selected.push(randomizer(uppers));
+    if (pUpper){ 
+        select.push(uppers.join(""));
     }
 
-    if (pNumber){
-        randos = randos.concat(numbers);  
-        selected.push(randomizer(numbers));
+    if (pNumber){  
+        select.push(numbers.join(""));
     }
 
-    if (pSpecial){
-        randos = randos.concat(specials);  
-        selected.push(randomizer(specials));
+    if (pSpecial){ 
+        select.push(specials.join(""));
     }
 
+    for (var i = 0; i < select.length; i++) {  
+        newChars.push(randomizer(select));
+
+    }
+        
     for (var i = 0; i < pLength; i++) {    
-        var randos = randomizer(randos);
-        newpassword.push(randos);
-    }
-
-    for (var i = 0; i < selected.length; i++) {    
-        newpassword[i] = selected[i];
+        newpassword[i] = newChars[i];
     }
 }
-    return newpassword.join("")
+
+    return newpassword
 }
 
 
@@ -79,6 +76,117 @@ function writePassword(){
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
 }
+
+
+
+
+
+
+
+
+
+
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
+
+
+
+// function randomizer(){
+//     var randoIdx = Math.floor(Math.random() * arr.length);
+//     var randoEl = arr[randoIdx];
+//     console.log(randoEl)
+//  }
+
+
+// function generatePassword(){
+//     var selected = []
+//     var newChars = []
+//     var newPword = []
+//     // var newpassword = []
+
+//     var pLength = prompt("What is the desired length of the password?")
+
+//     if (pLength < 8 || pLength > 128){ 
+//         alert("Must choose between 8 and 128 characters."); 
+//     }
+
+//     else {
+
+//     var pLower = confirm("Would you like lowercase characters included in the password")
+//     var pUpper = confirm("Would you like uppercase characters included in the password?")
+//     var pNumber = confirm("Would you like numeric characters included in the password?")
+//     var pSpecial = confirm("Would you like special characters included in the password?")
+    
+    
+//     lowers = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//     uppers = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//     numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+//     specials = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@"];
+
+//     if (!pLower && !pUpper && !pNumber && !pSpecial){
+//         alert("Must choose atleast one character type.");
+//     }
+
+//     if (pLower){
+//         selected.push(...lowers);
+//     }
+
+//     if (pUpper){ 
+//         selected.push(...uppers);
+//     }
+
+//     if (pNumber){
+//         selected.push(...numbers);
+//     }
+
+//     if (pSpecial){ 
+//         selected.push(...specials);
+//     }
+
+// }
+//     for (var i = 0; i < 79; i++){
+//         var selected = randomizer(selected);
+//     }
+    
+//     for (var i = 0; i < pLength; i++){
+//         newChars.push(selected);
+//     }
+
+//     for (var i = 0; i < pLength; i++){
+//         newPword.push(newChars);
+//         console.log(newPword);
+//     }
+
+//     // newPword.push(newchars);
+
+//     // newpword[i] = selected[i]
+// }
+
+
+
+//     // for (var i = 0; i < pLength; i++) {    
+//     //     var randos = randomizer(randos);
+//     //     newpassword.push(randos);
+//     // }
+
+//     // for (var i = 0; i < selected.length; i++) {    
+//     //     newpassword[i] = selected[i];
+//     // }
+
+//     // return newpassword.join("")
+
+
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+// // Write password to the #password input
+// function writePassword(){
+
+//     var password = generatePassword();
+//     var passwordText = document.querySelector("#password");
+//     passwordText.value = password;
+// }
 
 
 
@@ -161,13 +269,5 @@ function writePassword(){
 //         }
 //     }
 
-// } 
-//     // selected = []
-//     // for (var i = 0; i <= pLength; i++) {    
-//     // var selected = randos[Math.floor(Math.random() * pLength)];
-//     // console.log(selected)
-
-//     } 
-//     return newpassword.join("")
-
 // }
+//     return newpassword.join("")
